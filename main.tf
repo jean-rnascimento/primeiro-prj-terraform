d7d4b4bb0542d2385db2291e25ed9cd236556744
jean-rnascimento <jean.rnascimento@gmail.com>
2023-01-26 00:43:50 -0300

    criando projeto no git para mandar pra validação

---
+++ main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.25.2"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "jenkins" {
+  image    = "ubuntu-22-04-x64"
+  name     = "jenkins-vm1"
+  region   = var.region
+  size     = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.sshkey-primeiro-prj-terraform.id]
+}
+
+data "digitalocean_ssh_key" "sshkey-primeiro-prj-terraform" {
+  name = var.ssh_keys_name
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name    = "k8s"
+  region  = var.region
+  version = "1.24.8-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+  }
+}
+
+variable "do_token" {
+  default = ""
+}
+
+variable "ssh_keys_name" {
+  default = ""
+}
+
+variable "region" {
+  default = ""
+}
+
+output "output_jenkins_ip" {
+  value = digitalocean_droplet.jenkins.ipv4_address
+}
+
+resource "local_file" "foo" {
+  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+do_token      = "dop_v1_df1a7b2cb11a4a02e1cf187420cda83357a2aff5c7acf10a803c4ef69f9f82e9"
+ssh_keys_name = "sshkey-primeiro-prj-terraform"
+region        = "nyc1"
